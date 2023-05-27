63948725f12e9c4f952e94d32bb4bc61801960fe
Ivan Yanu <ivan.yanu@gmail.com>
2023-05-27 17:47:57 -0300

    Initial version of hipatia generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/hipatia/app/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.hipatia.app.config;
---
+++ src/main/java/com/hipatia/app/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.hipatia.app.repository;
+
+import com.hipatia.app.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
